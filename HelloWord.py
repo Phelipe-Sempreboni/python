2ba321d8d9ff733b5ff979474cdce2cd12b3f624
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2020-10-29 18:44:36 -0300

    Olá Mundo!
    Olá Mundo!

---
+++ HelloWord.py
@@ -0,0 +1,4 @@
+print ('Como sempre dizem, se você não começar uma linguagem de programação com o famoso "Olá Mundo", não conseguirá seguir em frente rsrsr')
+
+print('Olá Mundo! Bem vindo ao Python')
+
