daa48bf0b6e4c9cfe7b09b17d37e931c5b1b45b9
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2020-10-29 19:20:21 -0300

    Create logica_de_programacao.py

---
+++ Lógica de programação com python/logica_de_programacao.py
@@ -0,0 +1,14 @@
+#Script 1
+nome = 'Sempreboni'
+idade = 26
+print(f'Meu nome é {nome} e minha idade é {idade} anos')
+
+#Script 2
+def saudacao ():
+    nome = input('Qual seu nome? ')
+    print(f'Olá {nome}')
+saudacao()
+
+#Script 3
+
+
