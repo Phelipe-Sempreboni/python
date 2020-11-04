93c273da6f06f0c6e6709a700028f2b990a66a17
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2020-11-04 15:53:21 -0300

    Atualização de arquivos.
    Atualização de arquivos.

--- Conexões SQL Server x Python/conectar_sql_com_autenticao_sql_ao_python_maneiras.py
+++ Conexões SQL Server x Python/conectar_sql_com_autenticao_sql_ao_python_maneiras.py
@@ -1,14 +1,16 @@
 #Exemplo de conexão do Python com o SQL Server com autenticação do SQL Server, com necessidade de senha.
 #Neste caso, pode ser utilizado quando a autentição for pelo SQL Server, pois, nota-se que possui a linha 15 ("pwd=SENHA").
 
-#Nota: Não precisa ser executado com a função "def conectar_sql_server_com_autenticao_sql_ao_python()", se quiser pode ser retirada.
+#Nota: Não precisa ser executado com a função "def conectar()", se quiser pode ser retirada.
 #Nota2: Pode ser utilizado no Jupyter Notebook.
 #Nota3: Por ser login no SQL Server com (Autenticação SQL Server), a linha de código 16 ("Trusted_Connection=no;") necessita estar como "no", indicando o login por esse tipo de autenticação"
 
+#Maneira 1:
 
-def conectar_sql_server_com_autenticao_sql_ao_python():
-    import pyodbc
-    cnxn = pyodbc.connect(
+import pyodbc
+
+def conectar():
+    conexao = pyodbc.connect(
         "Driver={SQL Server Native Client 11.0};"
         "Server=SERVER;"
         "Database=DATABASENAME;"
@@ -16,4 +18,24 @@ def conectar_sql_server_com_autenticao_sql_ao_python():
         "pwd=SENHA;"
         "Trusted_Connection=no;"
     )
-conectar_sql_server_com_autenticao_sql_ao_python()
+conectar()
+
+#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#
+
+#Maneira 2:
+
+import pyodbc
+
+def conectar():
+    try:
+        conexao = pyodbc.connect(
+            "Driver={SQL Server Native Client 11.0};"
+            "Server=SERVER;"
+            "Database=DATABASENAME;"
+            "uid=USUARIO;"
+            "pwd=SENHA;"
+            "Trusted_Connection=no;"
+        )
+        return conexao.cursor()
+    except:
+        print('Não foi possível realizar a conexão com banco de dados.')
--- Conexões SQL Server x Python/conectar_sql_com_autenticao_windows_ao_python_maneiras.py
+++ Conexões SQL Server x Python/conectar_sql_com_autenticao_windows_ao_python_maneiras.py
@@ -1,17 +1,40 @@
 #Exemplo de conexão do Python com o SQL Server com autenticação do Windows, sem necessidade de senha.
 #Neste caso, pode ser utilizado quando a autentição for pelo Windows, pois, nota-se que não possui a password (senha) do database.
 
-#Nota: Não precisa ser executado com a função "def conectar_sql_server_com_autenticao_windows_ao_python()", se quiser pode ser retirada.
+#Nota: Não precisa ser executado com a função "def conectar()", se quiser pode ser retirada.
 #Nota2: Pode ser utilizado no Jupyter Notebook.
 #Nota3: Por ser login no SQL Server com (Autenticação Windows), a linha de código 14 ("Trusted_Connection=yes;") necessita estar como "yes", indicando o login por esse tipo de autenticação"
 
-def conectar_sql_server_com_autenticao_windows_ao_python():
-    import pyodbc
-    cnxn = pyodbc.connect(
+#Maneira 1:
+
+import pyodbc
+
+def conectar():
+
+    conexao = pyodbc.connect(
         "Driver={SQL Server Native Client 11.0};"
         "Server=SERVIDOR;"
         "Database=DATABASENAME;"
         "uid=USUARIO;"
         "Trusted_Connection=yes;"
     )
-conectar_sql_server_com_autenticao_windows_ao_python()
+conectar()
+
+#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#
+
+#Maneira 2:
+
+import pyodbc
+
+def conectar():
+    try:
+        conexao = pyodbc.connect(
+            "Driver={SQL Server Native Client 11.0};"
+            "Server=SERVER;"
+            "Database=DATABASENAME;"
+            "uid=USUARIO;"
+            "Trusted_Connection=yes;"
+        )
+        return conexao.cursor()
+    except:
+        print('Não foi possível realizar a conexão com banco de dados.')
