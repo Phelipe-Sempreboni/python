bb8281d85f9556c6bcde6d4776a65579ca125dc1
Luiz Phelipe Utiama Sempreboni <lsempreboni@uolinc.com>
2021-04-27 12:04:55 -0300

    Inserção de novos comandos para utilizar o CMD com Python
    Inserção de novos comandos para utilizar o CMD com Python

--- Python/Comandos com prompt de comando (CMD)/Script funcional.py
+++ Python/Comandos com prompt de comando (CMD)/Script funcional.py
@@ -17,3 +17,24 @@ subprocess.run('start', shell = True)
 # Nota: Abrir o cmd diretamente no terminal do Python.
 import os
 os.system('cmd')
+
+# --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # ---------  # ---------  # ---------  # --------- 
+
+# Executar comandos para verificação de diretórios.
+# Nota: Teremos o script inicial e um exemplo para verificar repositórios.
+# Observação: para todos os cripts abaixo é possível utilizar no lugar do (dir) o (cd) para acessar os diretórios.
+
+#Script inicial:
+# Este script executado da maneira abaixo, mostra os repositórios do local.
+import os
+os.system('dir')
+
+# Script de exemplo 1. Se por exemplo, for inserido o nome de um repositório e (.), ele mostrará todas as pastas ou arquivos dentro daquele repositório.
+import os
+os.system('dir Desktop.')
+
+#Script de exemplo 2. Se for por exemplo, for inserido o nome de um repositório e aopós (\nome_repositório), ele entrará neste repositório e mostrará todas as pastas ou arquivos dentro daquele repositório.
+import os
+os.system('dir Desktop\Temp')
+
+# --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # --------- # ---------  # ---------  # ---------  # ---------
