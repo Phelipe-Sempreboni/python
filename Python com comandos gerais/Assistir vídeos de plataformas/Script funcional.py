0201b76574126c7c86f80cab0983e2d413408ffc
Phelipe-Sempreboni <57469401+Phelipe-Sempreboni@users.noreply.github.com>
2021-07-29 10:55:07 -0300

    Criação de repositório de comandos gerais e inserção de scripts.

---
+++ Python com comandos gerais/Assistir vídeos de plataformas/Script funcional.py
@@ -0,0 +1,4 @@
+# Neste caso, iremos deixar um vídeo da Data Science Academy.
+# Este script deve ser 'rodado' no Jupyter Notebook ou Google Colab.
+from IPython.display import IFrame
+IFrame(src="https://www.youtube.com/embed/pu-pz77W1P8", width = "560", height = "315")
---
+++ Python com comandos gerais/Verificar versão do Python/Script funcional.py
@@ -0,0 +1,3 @@
+# Verificar versão do Python instalado.
+from platform import python_version
+print('Essa é versão',python_version(), 'do Python instalado')
