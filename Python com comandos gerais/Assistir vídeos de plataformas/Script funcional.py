7975ddd644eb34595599130a9708811ac240d2fb
Phelipe Sempreboni <57469401+Phelipe-Sempreboni@users.noreply.github.com>
2021-08-01 12:20:15 -0300

    Exclusão de repositório.

--- Python com comandos gerais/Assistir vídeos de plataformas/Script funcional.py
+++
@@ -1,4 +0,0 @@
-# Neste caso, iremos deixar um vídeo da Data Science Academy.
-# Este script deve ser 'rodado' no Jupyter Notebook ou Google Colab.
-from IPython.display import IFrame
-IFrame(src="https://www.youtube.com/embed/pu-pz77W1P8", width = "560", height = "315")
--- Python com comandos gerais/Verificar versão do Python/Script funcional.py
+++
@@ -1,3 +0,0 @@
-# Verificar versão do Python instalado.
-from platform import python_version
-print('Essa é versão',python_version(), 'do Python instalado')
