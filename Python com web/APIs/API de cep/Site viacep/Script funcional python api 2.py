892e79263ce135f6eeb5f3f42f8f58068520556b
Phelipe Sempreboni <57469401+Phelipe-Sempreboni@users.noreply.github.com>
2021-09-08 16:54:15 -0300

    Inserção de comentários no script.

--- Python com web/APIs/API de cep/Site viacep/Script funcional python api 2.py
+++ Python com web/APIs/API de cep/Site viacep/Script funcional python api 2.py
@@ -1,3 +1,14 @@
+'''
+Esse script acessa a API do site ViaCEP e nos retorna o resultado.
+
+Nesse script, será necessário indicar o CEP pesquisado no campo (request = requests.get("https://viacep.com.br/ws/06023090/json/")), onde uma vez alterado o número do CEP, será possível enxergar as informações.
+
+No script 1 desse repositório, temos a declaração de uma variável que que solicitar o CEP e retorna um resultado.
+
+Espero que essa API ajude.
+
+'''
+
 # Importação das bibliotecas.
 import requests
 import pandas as pd
