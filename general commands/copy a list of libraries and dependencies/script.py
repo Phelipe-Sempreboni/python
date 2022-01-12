84d2441e9303e60bd5dbdbd9a8262b9fa6782507
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-01-12 11:55:50 -0300

    Alteração do nome de repositório.

--- general commands/copy a list of libraries and dependencies/script.py
+++
@@ -1,5 +0,0 @@
-# Comando para exportar uma lista de bibliotecas e dependências de algum ambiente.
-
-# Notar que caso o arquivo já exista com certo nome no repositório que for realizar a exportação, então terá que alterar, conforme o exemplo abaixo.
-
-pip freeze > requirements_copia.txt
---
+++ general commands/export a list of libraries and dependencies/script.py
@@ -0,0 +1,5 @@
+# Comando para exportar uma lista de bibliotecas e dependências de algum ambiente.
+
+# Notar que caso o arquivo já exista com certo nome no repositório que for realizar a exportação, então terá que alterar, conforme o exemplo abaixo.
+
+pip freeze > requirements_copia.txt
