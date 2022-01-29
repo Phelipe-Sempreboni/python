8024e9a08c0ffeab9c8c618cac9fe9c88719becb
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-01-29 15:24:06 -0300

    Atualização de arquivo.
    - Atualizando o script desenvolvido chamado (script_version1.py).
    
    - Foram realizados testes e melhorias neste script.

--- projects/excel/read files and write data to database/script_version1.py
+++ projects/excel/read files and write data to database/script_version1.py
@@ -9,7 +9,8 @@ Last modification: 29/01/2022.
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
 # Importing libraries
-
+import os
+import openpyxl
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
@@ -28,4 +29,43 @@ employees_file_name = 'employee_registration.xlsx'
 employee_file_repository_path = (source_repository_path + '\\' + employees_file_name)
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+# Variável com o comando para abertura do arquivo excel que desejamos trabalhar, onde é feito com a biblioteca (openpyxl).
+wbk_employee = openpyxl.load_workbook(employee_file_repository_path)
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+# Variável com o comando para verificar todas as planilhas disponiveis que temos para trabalhar na tabela.
+wbk_employee_sheets_available = wbk_employee.sheetnames
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+# Variável com o comando para escolher a planilha (sheet) que iremos querer trabalhar.
+sheet_employee_registration = wbk_employee['employee_registration']
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+# Variável de lista que está vazia, pois, irá receber os valores da planilha que ficarão armazenados e posteriormente adicionados no banco de dados.
+# No final do script essa lista será esvaziada por questões de segurança, para não ocorrer erros e/ou acumulo de valores na próxima execução de script.
+lists = []
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+# O comando (iter_rows) realiza a iteração de comandos com as linhas.
+# O comando (iter_cols) realiza a iteração de comandos com as colunas.
+# Neste caso, estamos percorrendo a planilha, ou sheet, chamada ('employee_registration'), que está na variável (sheet_employee_registration).
+# Estamos percorrendo essa planilha, iniciando na linha minima 2, e lendo todos os valores.
+# Estamos utilizando o loop for para realizar a leitura completa dessa planilha, onde, para cada linha (row), retornamos os valores da instrução sheet_employee_registration.iter_rows(min_row=2, values_only=True).
+# Notar que teremos os valores devolvidos como tuplas, e tuplas são objetos imutáveis no python, ou seja, não é possível realizar nenhum tipo de modificação.
+for row in sheet_employee_registration.iter_rows(min_row=2, values_only=True):
+
+    lists.append(row)
+
+    tuples = tuple(lists)
+
+    print(tuples[0][1])
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+
 
