73cd18ba50ba1593a218be3c9297dc81ad7ff23d
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-01-29 13:25:30 -0300

    Atualização de arquivo.

--- projects/excel/read files and write data to database/script_version1.py
+++ projects/excel/read files and write data to database/script_version1.py
@@ -5,8 +5,21 @@ Created in: 29/01/2022.
 
 Last modification: 29/01/2022.
 '''
-import os
 
-for root, dicts, files in os.walk():
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
 
-    for file in files:
+# Importing libraries
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+# Variável com o nome do arquivo, que neste caso é fixo, mas pode ser alterado de acordo com a necessidade.
+employees_file_name = 'employee_registration.xlsx'
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
+# Variável com o caminho do repositório de origem.
+source_repository_path = r'C:\Users\pheli\OneDrive\Archives\2 - Github\Repositories\python\projects\excel\read files and write data to database\archives'
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------ #
+
