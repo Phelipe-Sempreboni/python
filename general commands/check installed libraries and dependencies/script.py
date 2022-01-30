cc4b0795d5a8c3cb1742aa8408d4401a13a5a00b
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-01-30 12:00:23 -0300

    Movimentação de repositório.
    - Repositório (check installed libraries and dependencies) foi movimentado para o repositório (libraries).

--- general commands/check installed libraries and dependencies/script.py
+++
@@ -1,3 +0,0 @@
-# Verificação das bibliotecas e dependências que estão instaladas no Python, tanto local ou no ambiente virtual (virtualenv).
-
-pip list
---
+++ libraries/check installed libraries and dependencies/script.py
@@ -0,0 +1,3 @@
+# Verificação das bibliotecas e dependências que estão instaladas no Python, tanto local ou no ambiente virtual (virtualenv).
+
+pip list
