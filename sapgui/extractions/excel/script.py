6df3cfca52a089f4ebd6de3b3bbe47abec6423a0
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2022-03-13 01:03:10 -0300

    Atualização de script.

--- sapgui/extractions/excel/script.py
+++ sapgui/extractions/excel/script.py
@@ -1,35 +1,31 @@
 '''
 
-created by: Luiz Phelipe Utiama Sempreboni
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------ /
 
-Funções do script:
-- Verificação se o script está realmente atuando no repositório apontado e não está tentando realizar a leitura de demais repositórios internos.
-- Verificação se existe qualquer tipo de arquivo dentro do repositório que foi apontado.
-- Verificação se existem arquivos com a extensão do tipo (xlsx/xlsm) do tipo excel.
-- Remoção (exclusão) apenas de arquivos com a extensão do tipo (xlsx/xlsm) do tipo excel do repositório que foi apontado.
-- Conexão com o SAP GUI com solicitação de usuário e senha para o usuário que estiver executando o comando.
+Created by: Luiz Phelipe Utiama Sempreboni
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------ /
 
-- Entrar na transação do SAP GUI para iniciar o processo, onde neste caso é uma transação ficticia.
-- Executar os comandos automatizados do SAP GUI pelo script.
-- Realizar a extração das bases de dados.
-- Salvar as bases de dados.
-- Finalizar o script.
+Função resumida do script:
+- Realizar uma conexão com o SAP GUI e extração de dados.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------ /
+
+Para mais informações sobre as funções do script, consulte o arquivo README.md deste repositório.
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------ \
 
 '''
-
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
 
 # Importação de módulos
 
 import win32com.client  # Módulo para criação de componentes COM (Common Object Model) em Python. Pode-se tanto criar componentes para serem utilizados por outras linguagens/aplicações (servidores) quanto criar objetos previamente existentes (clientes) criados em outras linguagens.
 import sys  # Módulo para fornecer funções e variáveis usadas para manipular diferentes partes do ambiente de tempo de execução do Python e apesar de serem completamente diferentes, muitas pessoas confundem o módulo sys e o módulo os (módulo para manipular o sistema operacional).
 import os  # Módulo para fornecer acesso a funções específicas do sistema para lidar com o sistema de arquivos, processos, planejador, etc.
-import glob  # Módulo usado para retornar todos os caminhos de arquivo que correspondem a um padrão específico.
 import subprocess  # Módulo subprocess permite que você execute programas externos e inspecione suas saídas com facilidade.
 import time  # Módulo provê várias funções relacionadas à tempo, onde neste caso estamos utilizando para a função (sleep).
 from datetime import datetime  # Módulo fornece classes para manipular datas e tempo de forma simples ou complexas. Apesar de cálculos aritméticos com data e tempo serem suportados, o foco da implementação está na extração eficiente de atributo para formatar resultados e manipulação.
-import getpass  # Módulo para mascarar a senha quando é inserida no terminal.
+
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
 
@@ -82,6 +78,87 @@ print('Iniciando o processo de extração de dados do SAP GUI.\n')
 print('=======================================================================================================================================================================\n')
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+
+# Função criada para realizar a conexão com o SAP GUI.
+def sap_connection():
+
+    # Bloco com (try) para tentarmos executar as ações abaixo.
+    try:
+
+        # Caminho onde o executável do SAP GUI está locado. Varia de máquina para máquina.
+        path = r"D:\Programming\SAP\FrontEnd\SapGui\saplogon.exe"
+
+        # Realiza a abertura de um processo, neste caso o processo de execução do executável do SAP GUI.
+        subprocess.Popen(path)
+
+        # Realiza uma contagem de 5 segundos até execução do processo abaixo.
+        time.sleep(5)
+
+        # Variável que define o objeto, neste caso, o (SAPGUI), para execução do objeto SAP GUI.
+        sap_gui_auto = win32com.client.GetObject('SAPGUI')
+
+        # Este (if) verifica se não existe nenhum tipo de objeto da variável (sap_gui_auto).
+        if not type(sap_gui_auto) == win32com.client.CDispatch:
+            return
+
+        # Aqui utilizamos a variável (sap_gui_auto) junto do método (GetScriptingEngine).
+        # Essa variável (application) executa a tecnologia, ou seja, o SAP GUI, fazendo com que o sistema seja aberto.
+        application = sap_gui_auto.GetScriptingEngine
+        if not type(application) == win32com.client.CDispatch:
+            sap_gui_auto = None
+            return sap_gui_auto
+
+        # Aqui utilizamos a variável (application) com o método (OpenConnection).
+        # Essa variável (connection) executa o módulo do SAP GUI, ou seja, o que você precisa realmente se conectar, por exemplo, CCS PRODUTIVO.
+        connection = application.OpenConnection("SAP", True)
+        if not type(connection) == win32com.client.CDispatch:
+            application = None
+            sap_gui_auto = None
+            return application, sap_gui_auto
+
+        session = connection.Children(0)
+        if not type(session) == win32com.client.CDispatch:
+            connection = None
+            application = None
+            sap_gui_auto = None
+            return connection, application, sap_gui_auto
+
+        # Comando interno gerado pelo script do SAP GUI para escrever o usuário na tela do sistema.
+        session.findById("wnd[0]/usr/txtRSYST-BNAME").text = 'LSEMPREBONI'
+
+        # Comando interno gerado pelo script do SAP GUI para escrever a senha na tela do sistema.
+        session.findById("wnd[0]/usr/pwdRSYST-BCODE").text = 'Estudar123+'
+
+        # Comanndo para entrar efetivamente no sistema após inserção do usuário e senha.
+        session.findById("wnd[0]").sendVKey(0)
+
+    # Caso ocorra qualquer tipo de erro no bloco deste (try), então será acionado o (except), impressa a mensagem abaixo e o programa será encerrado.
+    except Exception as error_1:
+
+        # Print para realizar as divisões entre as mensagens, visando deixar a leitura do usuário mais organizada.
+        print(
+            '\n=======================================================================================================================================================================\n')
+
+        # Mensagem impressa para na tela para o usuário.
+        print(f'Ocorreu um erro ao tentar realizar o processo de login no SAP GUI. '
+              f'\nPor favor, verifique e tente novamente.'
+              f'\nProcesso finalizado sem êxito.'
+              f'\nTipo do erro: {error_1.__class__}'
+              f'\nEssa tela será fechada em 10 segundos')
+
+        # Print para realizar as divisões entre as mensagens, visando deixar a leitura do usuário mais organizada.
+        print(
+            '\n=======================================================================================================================================================================\n')
+
+        # Pausar ou colocar para dormir a execução do script por 30 segundos até a execução do comando abaixo.
+        time.sleep(10)
+
+        # Caso ocorra o erro, então o script é encerrado e não prosseguirá para o próximo passo, assim evitando erros para o usuário posteriormente.
+        sys.exit()
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
+
 
 # Bloco de try para tentar realizar a execução abaixo.
 try:
@@ -244,68 +321,12 @@ try:
 
 # Este é o (except) do (try) geral, ou seja, é o que fecha o primeiro comando (try) aberto.
 # Caso nenhuma das verificações dentro do (try) geral for verdadeira, então irá ativar este (except).
-except Exception as error:
+except Exception as error_2:
 
     print(f'Não foi possível realizar o processo de validação de repositório e arquivos. '
           f'\nPor favor, verifique os seguintes pontos que podem estar causando o erro: '
           f'\n1 - Verifique se o repositório indicado no caminho da variável (nome_caminho) realmente existe. '
           f'\n2 - Verifique se existem arquivos com as extensões (xlsx/xlsm) no repositório. '
-          f'\n3 - Verifique se os arquivos não estão com erro de formato. \nTipo do erro: {error.__class__}\n')
+          f'\n3 - Verifique se os arquivos não estão com erro de formato. \nTipo do erro: {error_2.__class__}\n')
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
-
-
-# Função criada para realizar a conexão com o SAP GUI.
-def sap_connection():
-
-    try:
-
-        # Caminho onde o executável do SAP GUI está locado. Varia de máquina para máquina.
-        path = r"D:\Programming\SAP\FrontEnd\SapGui\saplogon.exe"
-
-        # Realiza a abertura de um processo, neste caso o processo de execução do executável do SAP GUI.
-        subprocess.Popen(path)
-
-        # Realiza uma contagem de 5 segundos até execução do processo abaixo.
-        time.sleep(5)
-
-        # Variável que define o objeto, neste caso, o (SAPGUI), para execução do objeto SAP GUI.
-        sap_gui_auto = win32com.client.GetObject('SAPGUI')
-
-        # Este (if) verifica se não existe nenhum tipo de objeto da variável (sap_gui_auto).
-        if not type(sap_gui_auto) == win32com.client.CDispatch:
-            return
-
-        # Aqui utilizamos a variável (sap_gui_auto) junto do método (GetScriptingEngine).
-        # Essa variável (application) executa a tecnologia, ou seja, o SAP GUI, fazendo com que o sistema seja aberto.
-        application = sap_gui_auto.GetScriptingEngine
-        if not type(application) == win32com.client.CDispatch:
-            sap_gui_auto = None
-            return sap_gui_auto
-
-        # Aqui utilizamos a variável (application) com o método (OpenConnection).
-        # Essa variável (connection) executa o módulo do SAP GUI, ou seja, o que você precisa realmente se conectar, por exemplo, CCS PRODUTIVO.
-        connection = application.OpenConnection("CCS PRODUTIVO", True)
-        if not type(connection) == win32com.client.CDispatch:
-            application = None
-            sap_gui_auto = None
-            return application, sap_gui_auto
-
-        session = connection.Children(0)
-        if not type(session) == win32com.client.CDispatch:
-            connection = None
-            application = None
-            sap_gui_auto = None
-            return connection, application, sap_gui_auto
-
-        #Variáveis de login e senha dos campos do SAP.
-        login = input('Informe seu login: ')
-        password = getpass.getpass('Informe sua senha: ')
-
-    except Exception as error_2:
-
-        print(f'erro {error_2.__class__}')
-
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
-
-
