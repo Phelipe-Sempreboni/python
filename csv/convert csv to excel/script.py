12364c77edc2736084beb37423714f347da34a80
Phelipe Sempreboni <57469401+Phelipe-Sempreboni@users.noreply.github.com>
2022-09-25 21:36:06 -0300

    Atualização de arquivol

--- csv/convert csv to excel/script.py
+++ csv/convert csv to excel/script.py
@@ -1,8 +1,24 @@
-# Método para converter um arquivo CSV em EXCEL.
+# Converter um arquivo CSV em Excel.
+# Lembrar de ter a biblioteca "pandas" e "openpyxl" instaladas, senão realize a instalação.
+# Comando para executar a instalação no Jupyter Notebook pip install pandas
+# Comando para executar a instalação no Jupyter Notebook pip install openpyxl
 
-# Importação da biblioteca.
+### Importação das bibliotecas.
+
 import pandas as pd
+import openpyxl
 
-data = pd.read_csv(r'') # Comando para leitura do arquivo csv. Insira o caminho do arquivo origem. 
+# Comando para leitura do arquivo CSV. Insira o caminho do arquivo origem.
+# Temos um exemplo de caminho inserido.
+archive_csv = pd.read_csv(r'C:\Users\USUARIO\Desktop\Temp\archive.csv')
 
-data.to_excel(r'', index=False) # Comando para converter o arquivo em Excel. Insira o caminho do arquivo destino.
+# Comando para converter o arquivo em Excel. Insira o caminho destino, ou seja, onde você quer que o arquivo seja salvo.
+# Temos um exemplo de caminho inserido.
+archive_csv.to_excel(r'C:\Users\USUARIO\Desktop\Temp\archive.csv', index=False)
+
+# Comando para leitura do arquivo Excel. Insira o caminho do arquivo origem.
+# Temos um exemplo de caminho inserido.
+archive_xlsx = pd.read_excel(r'C:\Users\USUARIO\Desktop\Temp\archive.xlsx')
+
+# Realizar a leitura das 5 primeiras linhas.
+archive_xlsx.head()
